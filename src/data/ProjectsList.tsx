2ed4483c559c1c6a00f7be136fb3754a5ecc4646
Emily Qi <emilyhlqi@gmail.com>
2026-04-13 01:13:23 -0400

    - not _

--- src/data/ProjectsList.tsx
+++ src/data/ProjectsList.tsx
@@ -13,7 +13,7 @@ const ProjectsList = [
     {
         type: 'software',
         title: 'Depth Estimation Benchmark',
-        link: 'https://github.com/emlyqi/depth_benchmarking',
+        link: 'https://github.com/emlyqi/depth-benchmarking',
         skills: ['python', 'pytorch', 'opencv', 'hugging face', 'onnx', 'numpy'],
         description: 'benchmarked stereo vs neural depth estimation, fine-tuned dpt-large, and wrote 6000 words about it',
         image: DepthBenchmarkPath
